9e60bebea7744847d16e9005dd4ca55c5826d426
sstha <34565072+sstha@users.noreply.github.com>
2023-05-26 23:57:02 -0700

    Update courses.js

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,16 +1,16 @@
 const courses = [
-     {
-       title: 'Convex Optimization',
-       number: 'EE 364a',
-       link: 'http://stanford.edu/class/ee364a/',
-       university: 'Stanford',
-     },
-     {
-       title: 'Machine Learning',
-       number: 'CS 229',
-       link: 'http://cs229.stanford.edu/',
-       university: 'Stanford',
-     },
+  {
+    title: 'Convex Optimization',
+    number: 'EE 364a',
+    link: 'http://stanford.edu/class/ee364a/',
+    university: 'Stanford',
+   },
+   {
+     title: 'Machine Learning',
+     number: 'CS 229',
+     link: 'http://cs229.stanford.edu/',
+     university: 'Stanford',
+   },
 //   {
 //     title: 'Computer Networks',
 //   },
