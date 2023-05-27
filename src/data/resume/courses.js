1ac5fb4ea5a1e4f5323d1dc21be6adf6ac91cbd6
sstha <34565072+sstha@users.noreply.github.com>
2023-05-27 00:00:24 -0700

    Update courses.js

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -4,40 +4,13 @@ const courses = [
     number: 'EE 364a',
     link: 'http://stanford.edu/class/ee364a/',
     university: 'Stanford',
-   },
-   {
-     title: 'Machine Learning',
-     number: 'CS 229',
-     link: 'http://cs229.stanford.edu/',
-     university: 'Stanford',
-   },
-//   {
-//     title: 'Computer Networks',
-//   },
-//   {
-//     title: 'Advanced Computer Networks',
-//   },
-//   {
-//     title: 'Distributed System',
-//   },
-//   {
-//     title: 'Database Processing',
-//   },
-//   {
-//     title: 'Computer Networks and Security',
-//   },
-//   {
-//     title: 'CyberCrime',
-//   },
-//   {
-//     title: 'Machine Learning',
-//   },
-//   {
-//     title: 'Artificial Intelligence',
-//   },
-//   {
-//     title: 'Software Engineering',
-//   },
+  },
+  {
+    title: 'Machine Learning',
+    number: 'CS 229',
+    link: 'http://cs229.stanford.edu/',
+    university: 'Stanford',
+  },
 ];
 
 export default courses;
