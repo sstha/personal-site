3f34ea48ea5ed70dafd16eda5f89f1518d348e1f
sstha <34565072+sstha@users.noreply.github.com>
2023-05-26 23:53:55 -0700

    Update courses.js

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,31 +1,43 @@
 const courses = [
-  {
-    title: 'Computer Networks',
-  },
-  {
-    title: 'Advanced Computer Networks',
-  },
-  {
-    title: 'Distributed System',
-  },
-  {
-    title: 'Database Processing',
-  },
-  {
-    title: 'Computer Networks and Security',
-  },
-  {
-    title: 'CyberCrime',
-  },
-  {
-    title: 'Machine Learning',
-  },
-  {
-    title: 'Artificial Intelligence',
-  },
-  {
-    title: 'Software Engineering',
-  },
+     {
+       title: 'Convex Optimization',
+       number: 'EE 364a',
+       link: 'http://stanford.edu/class/ee364a/',
+       university: 'Stanford',
+     },
+     {
+       title: 'Machine Learning',
+       number: 'CS 229',
+       link: 'http://cs229.stanford.edu/',
+       university: 'Stanford',
+     },
+//   {
+//     title: 'Computer Networks',
+//   },
+//   {
+//     title: 'Advanced Computer Networks',
+//   },
+//   {
+//     title: 'Distributed System',
+//   },
+//   {
+//     title: 'Database Processing',
+//   },
+//   {
+//     title: 'Computer Networks and Security',
+//   },
+//   {
+//     title: 'CyberCrime',
+//   },
+//   {
+//     title: 'Machine Learning',
+//   },
+//   {
+//     title: 'Artificial Intelligence',
+//   },
+//   {
+//     title: 'Software Engineering',
+//   },
 ];
 
 export default courses;
